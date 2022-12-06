79c18da8651f1e735da9ee03cb9f708256aea053
Nadav <nadavvivsa@gmail.com.com>
2022-12-06 16:50:35 +0200

    a

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -6,12 +6,12 @@ export const weatherService = {
 }
 
 async function getWeatherByLocationKey(locationKey) {
-    return await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=Em754KAbMWGXadsWl2QaZzXG9tfhYC9W&language=en-us&details=false`)
+    return await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=Em754KAbMWGXadsWl2QaZzXG9tfhYC9W&language=en-us&details=false`)
         .then(res => res.data[0])
         .catch(error => console.error('Error: try again later'))}
 
 
 async function getForecastByLocationKey(locationKey) {
-    return await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=Em754KAbMWGXadsWl2QaZzXG9tfhYC9W&language=en-us&details=false&metric=false`)
+    return await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=Em754KAbMWGXadsWl2QaZzXG9tfhYC9W&language=en-us&details=false&metric=false`)
         .then(res => res.data.DailyForecasts)
         .catch(error => console.error('Error: try again later'))}
