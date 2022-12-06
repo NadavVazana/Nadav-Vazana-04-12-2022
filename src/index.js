3de92c283d42ffb95bb16c8e1f2a875c809f8cc4
Nadav <nadavvivsa@gmail.com.com>
2022-12-06 16:30:49 +0200

    2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './store/store';
 import { Provider } from 'react-redux'
-import PlacesAutocomplete from 'react-places-autocomplete';
-import {
-    geocodeByAddress,
-    geocodeByPlaceId,
-    getLatLng,
-  } from 'react-places-autocomplete';
+
 import { App } from './App'
 import './assets/scss/style.scss'
 import { BrowserRouter as Router } from 'react-router-dom'
